5dab4a1e5c4cb0d5a9b1ef5461723c7c85d19ed8
Mateo Vergara Henao <mateovergara10@outlook.com>
2022-08-28 09:59:22 -0500

    Seeder Implementado

---
+++ Vehicles.API/Migrations/20220828144500_prueba3.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vehicles.API.Migrations
+{
+    public partial class prueba3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Vehicles.API/Program.cs
+++ Vehicles.API/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Hosting;
 using Vehicles.API.Data;
+using static System.Formats.Asn1.AsnWriter;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,7 @@ builder.Services.AddDbContext<DataContext>(x =>
     {
         x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
     });
+builder.Services.AddTransient<SeedDb>();
 
 WebApplication app = builder.Build();
 
@@ -16,11 +20,13 @@ WebApplication app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+SeedDatabase();
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -30,5 +36,14 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<SeedDb>();
+        dbInitializer.SeedAsync().Wait();
+    }
+}
 
 app.Run();
